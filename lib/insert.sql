c505e40adb5bcba0e675df0c841a9df6211194d6
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-26 01:44:34 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES 
+("Mr. Chocolate", 20, "M", "Brown", "Happy", "1"),
+("Rowdy", 10, "M", "Black", "Happy", "1"),
+("Tabitha", 15, "F", "Brown", "Happy", "1"),
+("Sergeant Brown", 25, "M", "Brown", "Happy", "1"),
+("Melissa", 23, "F", "Black", "Happy", "1"),
+("Grinch", 16, "M", "Brown", "Happy", "1"),
+("Wendy", 20, "F", "Brown", "Happy", "1"),
+(NULL, NULL, NULL, NULL, "Angry", NULL); 
