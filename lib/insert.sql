695433671ec5f70c214636e1626189e7486ffac7
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-26 01:14:09 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,17 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Mr. Chocolate", 20, "M", "Brown", "Happy", "1"); 
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 20, "M", "Brown", "Happy", "1"); 
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 20, "M", "Brown", "Happy", "1"); 
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 20, "M", "Brown", "Happy", "1"); 
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 20, "M", "Brown", "Happy", "1"); 
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 20, "M", "Brown", "Happy", "1"); 
